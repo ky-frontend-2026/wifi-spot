4c02f036a0a0a547313ae7e77c2b06503c4d01d4
lizb <loo4love90@gmail.com>
2026-02-27 10:46:18 +0900

    update

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className='rounded-2xl border bg-white p-6 shadow-sm'>
+      <h1>about</h1>
+      <p>about 페이지</p>
+    </div>
   )
 }
 
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const FavoritesPage = () => {
   return (
-    <div>FavoritesPage</div>
+    <div className='rounded-2xl border bg-white p-6 shadow-sm'>
+      <h1>Favorites</h1>
+      <p>즐겨찾기 기능</p>
+    </div>
   )
 }
 
